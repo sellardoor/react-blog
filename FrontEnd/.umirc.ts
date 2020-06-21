854ccc23070af0f28c5ac1b553986dc037b2714f
Jun <sellardoor@aliyun.com>
2020-06-21 12:53:16 +0800

    :bug: 删除测试文件

--- FrontEnd/.umirc.ts
+++ FrontEnd/.umirc.ts
@@ -17,7 +17,6 @@ export default defineConfig({
     { path: '/articlelist', component: '@/pages/ArticleList', exact: true },
     { path: '/message', component: '@/pages/Message', exact: true },
     { path: '/file', component: '@/pages/File', exact: true },
-    { path: '/test', component: '@/pages/Test', exact: true },
   ],
   externals: {
     [`highlight.js`]: 'window.hljs',
